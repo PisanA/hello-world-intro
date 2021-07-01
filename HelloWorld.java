08016d0af2d52e8bfbc65f2aaf4e43f9638fef36
margaret3991 <61333083+margaret3991@users.noreply.github.com>
2021-07-01 16:03:42 -0700

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,7 +5,7 @@
 class HelloWorld {
 
     public static void main(String[] args) {
-        int misspellinggg = 0;
+        int misspellingggg = 0;
         System.out.println("Hello, World!"); 
     }
 }
