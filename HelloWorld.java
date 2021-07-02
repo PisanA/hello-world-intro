879984becf5d01fe254f488aa3ea2101316900fc
margaret3991 <61333083+margaret3991@users.noreply.github.com>
2021-07-01 17:02:17 -0700

    Revert to starter code

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,7 +5,6 @@
 class HelloWorld {
 
     public static void main(String[] args) {
-        int misspellingggg = 0;
-        System.out.println("Hello, World!"); 
+        System.out.println("Hello, CSS Student!"); 
     }
 }
