087c577f8d1dbdaafb154afa5eb83c24882963b4
margaret3991 <61333083+margaret3991@users.noreply.github.com>
2021-07-06 16:42:55 -0700

    removing test comment

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,6 @@
 
 
 class HelloWorld {
-    // comment mispellinggg exsiting testt  sdfsdfs dsdf
     public static void main(String[] args) {
         System.out.println("Hello, CSS 142 Student!"); 
     }
